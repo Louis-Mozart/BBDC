e525bf712544d663194233b40b021d10e7dea032
Louis Mozart KAMDEM TEYOU <93260304+Louis-Mozart@users.noreply.github.com>
2026-06-25 19:32:46 +0200

    Refactor score.py for better readability
    Removed unnecessary blank lines and improved code formatting.

--- score.py
+++ score.py
@@ -4,7 +4,6 @@ def compute_accuracy(dat1,dat2,prof_skeleton):
     acc_context = 0
     n_affect_true = 0
     n_context_true = 0
-    
     for i in range(len(prof_skeleton)):
         if prof_skeleton["affect"][i] == True:
             n_affect_true += 1
@@ -16,6 +15,5 @@ def compute_accuracy(dat1,dat2,prof_skeleton):
             if dat1["context"][i] == dat2["context"][i]:
                 acc_context+=1
     acc = (acc_affect/n_affect_true + acc_context/n_context_true)/2
-
     return acc
 
